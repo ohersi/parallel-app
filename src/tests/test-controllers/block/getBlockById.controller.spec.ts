ff5edcb63dbdf905859100bbbedc8b4eac1ebe20
Omar Hersi <o.hersi@yahoo.com>
2023-04-23 20:55:33 -0400

    Add tests for getBlockById usecase and controller

---
+++ src/tests/test-controllers/block/getBlockById.controller.spec.ts
@@ -0,0 +1,97 @@
+//Packages
+import { mockDeep, objectContainsKey } from "jest-mock-extended";
+import { mockReset } from "jest-mock-extended/lib/Mock";
+import { Application, NextFunction, Request, Response } from 'express';
+import request from "supertest";
+import { cleanUpMetadata } from "inversify-express-utils";
+// Imports
+import GetBlockByIdController from "../../../controllers/block/getBlockById.controller";
+import GetBlockByIdUsecase from "../../../services/usecases/block/getBlockByID.usecase";
+import { start } from '../../../app';
+
+describe("GetChannelByIdController", () => {
+    // Mocks
+    const mockedUsecase = mockDeep<GetBlockByIdUsecase>();
+    const requestMock = mockDeep<Request>();
+    const responseMock = mockDeep<Response>();
+    const nextMock = mockDeep<NextFunction>();
+    // System Under Test (sut)
+    let controller: GetBlockByIdController;
+
+    // Supertest setup
+    let app: Application;
+
+    beforeAll(async () => {
+        const res = await start(5000);
+        app = res.build();
+        return app;
+    })
+
+    beforeEach(() => {
+        controller = new GetBlockByIdController(mockedUsecase);
+        mockReset(mockedUsecase);
+        // Inversify clean up existing metadata
+        cleanUpMetadata();
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    })
+
+    afterAll(() => {
+        //TODO: Close server --- server.close() or w/e
+    })
+
+    it("should be defined", () => {
+        expect(controller).toBeDefined();
+    })
+
+    describe("When getting a block by id,", () => {
+
+        describe("and the block corresponding to the id is found,", () => {
+
+            describe("and block object is retrieved,", () => {
+
+                it("return a block object and status of 200.", async () => {
+                    // GIVEN
+                    const id = 1;
+                    // WHEN
+                    const results = await request(app).get(`/api/v1/blocks/${id}`);
+
+                    // THEN
+                    expect(results.status).toEqual(200);
+                })
+            })
+
+            describe("and block object is empty,", () => {
+
+                it("return a status of 500.", async () => {
+                    // GIVEN
+                    const id = 1;
+
+                    // WHEN
+                    mockedUsecase.execute.mockResolvedValue([]);
+                    await controller.getBlockByID(requestMock, responseMock, nextMock);
+
+                    // THEN
+                    expect(responseMock.status).toBeCalledWith(500);
+                    expect(responseMock.send).toBeCalledWith(objectContainsKey('error'));
+                })
+            })
+        })
+
+        describe("and the block corresponding to the id is not found,", () => {
+
+            it("return a status of 500.", async () => {
+                // GIVEN
+                const id = -99;
+                // WHEN
+                const results = await request(app).get(`/api/v1/blocks/${id}`);
+
+                // THEN
+                expect(results.status).toEqual(500);
+            })
+        });
+
+    });
+});
---
+++ src/tests/test-services/block/getBlockById.usecase.spec.ts
@@ -0,0 +1,100 @@
+// Packages
+import { Connection, IDatabaseDriver, MikroORM } from "@mikro-orm/core";
+import { mockDeep } from "jest-mock-extended";
+import { mockReset } from "jest-mock-extended/lib/Mock";
+import { cleanUpMetadata } from "inversify-express-utils";
+import { IBackup } from 'pg-mem';
+// Imports
+import { memOrm } from "../../test-utils/init-db.setup";
+import { Block } from "../../../entities/block.entity";
+import { generateItems } from "../../test-utils/generate-items.setup";
+import BlockRepository from "../../../repositories/block.repository";
+import GetBlockByIdUsecase from "../../../services/usecases/block/getBlockByID.usecase";
+import BlockException from "../../../utils/exceptions/block.exception";
+
+describe("GetBlockByIdUsecase", () => {
+
+    const mockedBlockRepo = mockDeep<BlockRepository>();
+    let service: GetBlockByIdUsecase;
+    let orm: MikroORM<IDatabaseDriver<Connection>>;
+    let backup: IBackup;
+
+    beforeEach(() => {
+        service = new GetBlockByIdUsecase(mockedBlockRepo);
+        // Restore im-mem db to original state
+        backup.restore();
+        mockReset(mockedBlockRepo);
+        cleanUpMetadata();
+    })
+
+    beforeAll(async () => {
+        // Create database instance
+        const execute = await memOrm;
+        orm = execute.memOrm;
+        // Generate test entities
+        await generateItems(orm);
+        // Create backup of db
+        backup = execute.memDb.backup();
+    });
+
+    afterAll(async () => {
+        await orm.close();
+    })
+
+    it("should be defined", () => {
+        expect(service).toBeDefined();
+    })
+
+    describe('When given a block id,', () => {
+
+        describe("and the block does NOT exist in the db,", () => {
+
+            it("return null.", async () => {
+                // GIVEN
+                const blockID = -99;
+
+                // WHEN
+                const getBlock = await orm.em.find(Block, { id: blockID }, { populate: ['channels'] });
+                mockedBlockRepo.getBlockAndItsChannels.mockResolvedValue(getBlock);
+
+                const results = await service.execute(blockID);
+
+                // THEN
+                expect(getBlock).toEqual([]);
+                expect(results).toEqual([]);
+            })
+        })
+
+        describe("and the block does exist in the db,", () => {
+
+            it("return a block object from database.", async () => {
+                // GIVEN
+                const blockID = 1;
+
+                // WHEN
+                const getBlock = await orm.em.find(Block, { id: blockID }, { populate: ['channels'] });
+                mockedBlockRepo.getBlockAndItsChannels.mockResolvedValue(getBlock);
+
+                const results = await service.execute(blockID);
+
+                // THEN
+                expect(getBlock).toEqual(results);
+            })
+        })
+
+        describe("and db throws an error,", () => {
+
+            it("return the thrown error.", async () => {
+                // GIVEN
+                const id = -99;
+
+                // WHEN
+                mockedBlockRepo.getBlockAndItsChannels.mockRejectedValue(Error);
+
+                // THEN
+                expect(async () => { await service.execute(id) }).rejects.toThrow(BlockException);
+            })
+        })
+
+    })
+});
