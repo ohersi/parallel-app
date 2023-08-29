9aacbbb7452389fd5bdf9793f0631cef5e825c5b
Omar Hersi <o.hersi@yahoo.com>
2023-08-28 20:09:43 -0400

    Add external redis database connection

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,16 @@ import 'reflect-metadata';
 // Register path aliases - Needs to be at top of file
 import 'module-alias/register';
 // Packages
+import { Connection, IDatabaseDriver, MikroORM, RequestContext } from '@mikro-orm/core';
 import express, { Application, NextFunction } from 'express';
-import cors from 'cors';
-import compression from 'compression';
-import helmet from 'helmet';
-import session from 'express-session';
+import { InversifyExpressServer } from 'inversify-express-utils';
+import { Container } from 'inversify';
 import { Redis } from 'ioredis';
 import RedisStore from 'connect-redis';
-import { Container } from 'inversify';
-import { InversifyExpressServer } from 'inversify-express-utils';
-import { Connection, IDatabaseDriver, MikroORM, RequestContext } from '@mikro-orm/core';
+import session from 'express-session';
+import compression from 'compression';
+import helmet from 'helmet';
+import cors from 'cors';
 // Imports
 import ErrorMiddleware from '@/middleware/error.middleware';
 import initContainer from '@/utils/ioc/di-container';
@@ -25,7 +25,7 @@ export const redisContainer = {} as {
 export const start = async (port: Number) => {
 
     //Initalize Redis client
-    const redisClient = new Redis();
+    const redisClient = process.env.REDIS_DB ? new Redis(process.env.REDIS_DB) : new Redis();
     // Export redis for entire application
     redisContainer.redis = redisClient;
     // Initalize Redis store
@@ -50,14 +50,13 @@ export const start = async (port: Number) => {
 
         // Initalize CORS
         //TODO: Replace w/ approved site/s (array's require Origin header, one origin does not)
-        const allowedOrigins = process.env.WHITELIST_ORIGINS;  
-        
+        const allowedOrigins = process.env.WHITELIST_ORIGINS;
+
         const options: cors.CorsOptions = {
             origin: allowedOrigins,
             optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
             credentials: true
         };
-        // app.options('*', cors(options));
         app.use(cors(options));
 
         // Check for secret key & setup session
@@ -69,8 +68,7 @@ export const start = async (port: Number) => {
             saveUninitialized: false,
             cookie: {
                 httpOnly: true,
-                // secure: process.env.NODE_ENV === 'production' ? true : "auto",
-                // sameSite: process.env.NODE_ENV === 'production' ? "none" : "lax", 
+                // secure: process.env.NODE_ENV === 'production' ? true : false,
                 maxAge: 60 * 60 * 1000 // Expires in 1hr
             }
         }))
